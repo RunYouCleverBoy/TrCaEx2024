d0222eca76cf12d5cb3f43b73f0da9b789d3634e
Shmuel Meymann <meymann@gmail.com>
2024-01-27 21:40:44 +0200

    Padding

--- app/src/main/java/com/playgrounds/sitescraper/ui/screens/main/MainScreen.kt
+++ app/src/main/java/com/playgrounds/sitescraper/ui/screens/main/MainScreen.kt
@@ -2,6 +2,7 @@ package com.playgrounds.sitescraper.ui.screens.main
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -10,12 +11,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
 import com.playgrounds.sitescraper.R
 import com.playgrounds.sitescraper.ui.screens.main.models.MainState
 
 @Composable
 fun MainScreen(state: MainState) {
-    LazyColumn(modifier = Modifier.fillMaxWidth()) {
+    LazyColumn(modifier = Modifier.fillMaxWidth().padding(all = 16.dp)) {
         item {
             Column(
                 modifier = Modifier.fillMaxWidth()
