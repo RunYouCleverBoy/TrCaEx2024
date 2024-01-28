70f3c8643e7f9a08d01516c37045aaec72f5c9d6
Shmuel Meymann <meymann@gmail.com>
2024-01-28 14:40:19 +0200

    Formatted the title bar

--- app/src/main/java/com/playgrounds/sitescraper/ui/components/TopBar.kt
+++ app/src/main/java/com/playgrounds/sitescraper/ui/components/TopBar.kt
@@ -3,22 +3,28 @@ package com.playgrounds.sitescraper.ui.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.playgrounds.sitescraper.R
 
 @Composable
 fun TopBar(onRefresh: () -> Unit = {}) {
-    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-        Text(text = stringResource(R.string.top_bar_title))
+    Row(modifier = Modifier.fillMaxWidth().padding(16.dp), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
+        Text(text = stringResource(R.string.top_bar_title), style = MaterialTheme.typography.titleLarge)
         IconButton(onClick = onRefresh) {
-            Icon(painter = painterResource(id = R.drawable.baseline_refresh_24), contentDescription = stringResource(
+            Icon(imageVector = Icons.Default.Refresh, contentDescription = stringResource(
                 id = R.string.refresh_button_description)
             )
         }
